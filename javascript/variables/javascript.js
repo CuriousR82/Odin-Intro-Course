71a1edceb833fbefbcaaaab24eda2321c628bd4a
CuriousR82 <rosajeon82@gmail.com>
2022-08-08 18:03:01 -0400

    start of js content

---
+++ javascript/variables/javascript.js
@@ -0,0 +1,15 @@
+let message; // creates a var named "message" 
+message = 'Hello!';
+alert(message); // shows the variable content
+
+message = 'World!'; // value changed
+alert(message); 
+
+// a variable should be declared only once, repeated declaration ends in an error
+
+// var names are made of letters, numbers, and _, but they cant start w numbers, and multiple words are showcased in camelCase
+
+// the names are case-sensitive
+
+const myBirthday = '2004/08/02'; // unchanging variable
+
